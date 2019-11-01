1cb53e697bc5f67a4a2a47e59c75e34807f42ccb
afmika <afmichael73@gmail.com>
2019-11-01 19:03:51 +0300

    SimpleBot test

--- simplebot.js
+++ simplebot.js
@@ -17,7 +17,7 @@ nlp.addOutput("en/thanks", "You're welcome ;)");
 nlp.addOutput("en/thanks", "Nah that's fine");
 
 
-// test with NEW sentence
+// test with a NEW sentence
 let user_input = "thank ya";
 console.log(nlp.classify(user_input));
 console.log("Output", nlp.guessResponse(user_input));
